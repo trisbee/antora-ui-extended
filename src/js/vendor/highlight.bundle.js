039685fc56f5e2fd67e39a9e9b23edebd3199d2d
Michal Landsman <landsman@insuit.cz>
2021-01-05 20:56:57 +0100

    highlight http request

--- src/js/vendor/highlight.bundle.js
+++ src/js/vendor/highlight.bundle.js
@@ -1,12 +1,12 @@
 ;(function () {
   'use strict'
 
-  var hljs = require('highlight.js/lib/highlight')
+  var hljs = require('highlight.js/lib/core')
   hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
+  hljs.registerLanguage('http', require('highlight.js/lib/languages/http'))
   hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
   hljs.registerLanguage('clojure', require('highlight.js/lib/languages/clojure'))
   hljs.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'))
-  hljs.registerLanguage('cs', require('highlight.js/lib/languages/cs'))
   hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
   hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'))
   hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
